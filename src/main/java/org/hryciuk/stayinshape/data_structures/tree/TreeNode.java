c1916aea122bf39e66812a21eeb34e8a52f983d2
Piotr Hryciuk <piotrek.hryciuk@gmail.com>
2020-05-04 10:55:23 +0200

    Search value in a Binary Search Tree

--- src/main/java/org/hryciuk/stayinshape/data_structures/tree/TreeNode.java
+++ src/main/java/org/hryciuk/stayinshape/data_structures/tree/TreeNode.java
@@ -4,26 +4,32 @@ import java.util.Objects;
 
 class TreeNode {
 
-  Integer val;
-  TreeNode left;
-  TreeNode right;
+    Integer val;
+    TreeNode left;
+    TreeNode right;
 
-  TreeNode(Integer val) {
-    this.val = val;
-  }
+    TreeNode(Integer val) {
+        this.val = val;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    TreeNode treeNode = (TreeNode) o;
-    return Objects.equals(val, treeNode.val) &&
-            Objects.equals(left, treeNode.left) &&
-            Objects.equals(right, treeNode.right);
-  }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(val, left, right);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TreeNode treeNode = (TreeNode) o;
+        return Objects.equals(val, treeNode.val) &&
+                Objects.equals(left, treeNode.left) &&
+                Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
+    }
 }
