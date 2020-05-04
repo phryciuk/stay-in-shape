9d20966e2f63b75ec25b64db94558caa34105642
Piotr Hryciuk <piotrek.hryciuk@gmail.com>
2020-05-04 22:08:13 +0200

    Palindrome partitioning - test

--- src/main/java/org/hryciuk/stayinshape/strings/PalindromePartitioning.java
+++ src/main/java/org/hryciuk/stayinshape/strings/PalindromePartitioning.java
@@ -7,18 +7,18 @@ import java.util.List;
 /**
  * 131. Palindrome Partitioning
  * Medium
- *
+ * <p>
  * Given a string s, partition s such that every substring of the partition is a palindrome.
- *
+ * <p>
  * Return all possible palindrome partitioning of s.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Input: "aab"
  * Output:
  * [
- *   ["aa","b"],
- *   ["a","a","b"]
+ * ["aa","b"],
+ * ["a","a","b"]
  * ]
  */
 public class PalindromePartitioning {
@@ -27,22 +27,32 @@ public class PalindromePartitioning {
             return new ArrayList<>();
         }
         ArrayList<List<String>> result = new ArrayList<>();
-        if (s.length() == 1) {
-            List<String> singlePartition = Arrays.asList(s);
-            result.add(singlePartition);
-            return result;
-        }
+        partition(result, new ArrayList<>(), 0, s);
         return result;
     }
 
-    private boolean isPalindrome(String toTest) {
+    private void partition(List<List<String>> result, List<String> tempList, int position, String s) {
+        if (s.length() == position) {
+            result.add(new ArrayList<>(tempList));
+        }
+        for (int i = position; i < s.length(); ++i) {
+            if (isPalindrome(s, position, i+1)) {
+                tempList.add(s.substring(position, i+1));
+                partition(result, tempList, i+1, s);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String toTest, int beginning, int ending) {
         if (toTest == null || toTest.isEmpty()) {
             return false;
         }
         if (toTest.length() == 1) {
             return true;
         }
-        char[] chars = toTest.toCharArray();
+        String substring = toTest.substring(beginning, ending);
+        char[] chars = substring.toCharArray();
         for (int i = 0; i < chars.length / 2; ++i) {
             if (chars[i] != chars[chars.length - 1 - i]) {
                 return false;
--- src/test/groovy/org/hryciuk/stayinshape/strings/PalindromePartitioningTest.groovy
+++ src/test/groovy/org/hryciuk/stayinshape/strings/PalindromePartitioningTest.groovy
@@ -5,10 +5,13 @@ import spock.lang.Specification
 class PalindromePartitioningTest extends Specification {
     def "should return all palindromic partitions"(String input, String[][] expectedCuts) {
         given:
+        PalindromePartitioning palindromePartitioning = new PalindromePartitioning();
 
         when:
+        String[][] actual = palindromePartitioning.partition(input)
 
         then:
+        Arrays.equals(actual, expectedCuts)
 
         where:
         input         | expectedCuts
