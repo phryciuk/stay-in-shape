a09e55bbd2438bf31323047db5ee7ebbf9a80672
Piotr Hryciuk <piotrek.hryciuk@gmail.com>
2020-05-06 10:35:22 +0200

    Letter case permutation - better version

--- src/main/java/org/hryciuk/stayinshape/backtracking/LetterCasePermutation.java
+++ src/main/java/org/hryciuk/stayinshape/backtracking/LetterCasePermutation.java
@@ -9,30 +9,26 @@ public class LetterCasePermutation {
             return new ArrayList<>();
         }
         List<String> result = new ArrayList<>();
-        permute(s, result, 0);
+        char[] chars = s.toCharArray();
+        permute(chars, result, 0);
         return result;
     }
 
-    private void permute(String s, List<String> result, int position) {
-        if (position == s.length() && !result.contains(s)) {
-            result.add(s);
+    private void permute(char[] string, List<String> result, int index) {
+        if (index == string.length) {
+            result.add(new String(string));
+            return;
         }
-        for (int i = position; i < s.length(); i++) {
-            if (!Character.isDigit(s.charAt(i))) {
-                String first = s.substring(0, i) + Character.toUpperCase(s.charAt(i)) + s.substring(i + 1);
-                if (!first.equals(s)) {
-                    permute(first, result, i + 1);
-                }
-                String second = s.substring(0, i) + Character.toLowerCase(s.charAt(i)) + s.substring(i + 1);
-                if (!second.equals(s)) {
-                    permute(second, result, i + 1);
-                }
-            } else {
-                permute(s, result, i + 1);
-            }
-        }
-        if (!result.contains(s)) {
-            result.add(s);
+        if (Character.isDigit(string[index])) {
+            permute(string, result, index + 1);
+        } else {
+            char character = string[index];
+            char lowerCase = Character.toLowerCase(character);
+            string[index] = lowerCase;
+            permute(string, result, index + 1);
+            char upperCase = Character.toUpperCase(character);
+            string[index] = upperCase;
+            permute(string, result, index + 1);
         }
     }
 }
