5bd2f02de3e4aef82382fb0386c9ec9dbf05c72c
Piotr Hryciuk <piotrek.hryciuk@gmail.com>
2020-05-15 13:33:26 +0200

    Binary tree - pre-order traversal iteratively

--- src/main/java/org/hryciuk/stayinshape/data_structures/tree/BinaryTreePreorderTraversal.java
+++ src/main/java/org/hryciuk/stayinshape/data_structures/tree/BinaryTreePreorderTraversal.java
@@ -2,8 +2,35 @@ package org.hryciuk.stayinshape.data_structures.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePreorderTraversal {
+
+    // iteratively
+    public List<Integer> preorderTraversalIteratively(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        if (root.left == null && root.right == null) {
+            result.add(root.val);
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (!stack.isEmpty() || current != null) {
+            if (current != null) {
+                result.add(current.val);
+                stack.push(current);
+                current = current.left;
+            } else {
+                TreeNode popped = stack.pop();
+                current = popped.right;
+            }
+        }
+        return result;
+    }
+
     public List<Integer> preorderTraversal(TreeNode root) {
         if (root == null) {
             return new ArrayList<>();
