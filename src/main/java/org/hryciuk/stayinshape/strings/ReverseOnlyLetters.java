97cce555aba359a8d0045347fe932e1e8ed71f5c
Piotr Hryciuk <piotrek.hryciuk@gmail.com>
2020-06-17 08:31:05 +0200

    Reverse Only Letters - improved

--- src/main/java/org/hryciuk/stayinshape/strings/ReverseOnlyLetters.java
+++ src/main/java/org/hryciuk/stayinshape/strings/ReverseOnlyLetters.java
@@ -27,17 +27,11 @@ public class ReverseOnlyLetters {
     public String reverseOnlyLetters(String S) {
         char[] chars = S.toCharArray();
         for (int i = 0, j = chars.length - 1; i < j; ++i, --j) {
-            while (!Character.isLetter(chars[i]) && i < chars.length - 1) {
+            while (!Character.isLetter(chars[i]) && i < j) {
                 i++;
             }
-            if (i > j) {
-                break;
-            }
-            while (!Character.isLetter(chars[j]) && j > 0) {
+            while (!Character.isLetter(chars[j]) && i < j) {
                 j--;
-            }
-            if (i > j) {
-                break;
             }
             if (Character.isLetter(chars[i]) && Character.isLetter(chars[j])) {
                 char tmp = chars[i];
