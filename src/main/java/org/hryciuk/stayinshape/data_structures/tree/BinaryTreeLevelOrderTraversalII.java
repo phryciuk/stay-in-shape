e35e4043a0016bcdb255ec9ae5ab10f3b19b7dd9
Piotr Hryciuk <piotrek.hryciuk@gmail.com>
2020-06-30 14:40:17 +0200

    Binary Tree Level Order Traversal II

---
+++ src/main/java/org/hryciuk/stayinshape/data_structures/tree/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,68 @@
+package org.hryciuk.stayinshape.data_structures.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 107. Binary Tree Level Order Traversal II
+ * Easy
+ *
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+ *
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * return its bottom-up level order traversal as:
+ *
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ */
+public class BinaryTreeLevelOrderTraversalII {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            List<Integer> levelList = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode polled = q.poll();
+                levelList.add(polled.val);
+                if (polled.left != null) {
+                    q.offer(polled.left);
+                }
+                if (polled.right != null) {
+                    q.offer(polled.right);
+                }
+            }
+            result.add(levelList);
+        }
+        reverse(result);
+        return result;
+    }
+
+    void reverse(List<List<Integer>> list) {
+        for (int i = 0; i < list.size() / 2; ++i) {
+            List<Integer> head = list.get(i);
+            List<Integer> tail = list.get(list.size() - 1 - i);
+            List<Integer> tmp = head;
+            list.set(i, tail);
+            list.set(list.size() - 1 - i, tmp);
+        }
+    }
+}
