301f33007ac9c9c1d6afe26744c6d612a73f109d
Piotr Hryciuk <piotrek.hryciuk@gmail.com>
2020-08-05 15:25:38 +0200

    Lowest Common Ancestor - other versions

--- src/main/java/org/hryciuk/stayinshape/data_structures/tree/LowestCommonAncestorOfABinaryTree.java
+++ src/main/java/org/hryciuk/stayinshape/data_structures/tree/LowestCommonAncestorOfABinaryTree.java
@@ -1,6 +1,62 @@
 package org.hryciuk.stayinshape.data_structures.tree;
 
+import java.util.LinkedList;
+
 public class LowestCommonAncestorOfABinaryTree {
+
+    public TreeNode lowestCommonAncestorSimplest(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null ) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestorSimplest(root.left, p, q);
+        TreeNode right = lowestCommonAncestorSimplest(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+
+
+    public TreeNode lowestCommonAncestorSimpler(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        LinkedList<TreeNode> pPath = new LinkedList<>();
+        LinkedList<TreeNode> qPath = new LinkedList<>();
+        hasPath(root, p, pPath);
+        hasPath(root, q, qPath);
+        int index = 0;
+        while (index < pPath.size() && index < qPath.size()) {
+            TreeNode pCurrent = pPath.get(index);
+            TreeNode qCurrent = qPath.get(index);
+            if (pCurrent != qCurrent) {
+                break;
+            }
+            index++;
+        }
+        return pPath.get(index - 1);
+    }
+
+    private boolean hasPath(TreeNode root, TreeNode toFind, LinkedList<TreeNode> path) {
+        if (root == null) {
+            return false;
+        }
+        if (root == toFind) {
+            path.addFirst(root);
+            return true;
+        }
+        boolean left = hasPath(root.left, toFind, path);
+        boolean right = hasPath(root.right, toFind, path);
+        if (left || right) {
+            path.addFirst(root);
+            return true;
+        }
+        return false;
+    }
+
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root.val == p.val || root.val == q.val) {
